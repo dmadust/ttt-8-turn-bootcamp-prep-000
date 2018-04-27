11367a906e2e015c95904dab5c3a7828755abe33
dmadust@gmail.com <dmadust@gmail.com>
2018-04-27 19:34:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,5 +31,9 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valiud_move?(board)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    turn(board)
+  end
 end
