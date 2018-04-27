63b1ece69193a0932db5f3746fbacf78acfc7ed5
dmadust@gmail.com <dmadust@gmail.com>
2018-04-27 19:14:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,10 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def valid_move?(board, index)
   return index.between?(0, 8) && !position_taken?(board, index)
 end
 
+
 def position_taken?(board, index)
   return board[index] == 'X' || board[index] == 'O'
+end
+
+
+def move(board, index, token='X')
+  board[index] = token
 end
